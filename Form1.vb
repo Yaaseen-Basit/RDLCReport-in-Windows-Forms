a2831839afd38537f684935b681d7c7208694df1
Yaaseen-Basit <44226750+Yaaseen-Basit@users.noreply.github.com>
2020-09-25 18:49:46 +0530

    RDLC Report in Windows Forms

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
+    End Sub
+End Class
